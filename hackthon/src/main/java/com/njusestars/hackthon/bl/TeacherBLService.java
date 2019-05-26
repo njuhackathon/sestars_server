50b42dc67d2ed0fb98a7c49ad20c978e661e1f76
njulzb <hellolizebin@foxmail.com>
2019-05-26 10:43:24 +0800

    添加service框架

---
+++ hackthon/src/main/java/com/njusestars/hackthon/bl/TeacherBLService.java
@@ -0,0 +1,11 @@
+package com.njusestars.hackthon.bl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lzb
+ * @date 2019/5/26 10:39
+ */
+@Service
+public interface TeacherBLService {
+}
