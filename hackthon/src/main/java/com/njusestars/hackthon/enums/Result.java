ccb7a999065f7d2074d29c29928578497a340d7d
Xunner <161250042@smail.nju.edu.cn>
2019-05-26 11:28:35 +0800

    backend: add Result enum and UserBLService

---
+++ hackthon/src/main/java/com/njusestars/hackthon/enums/Result.java
@@ -0,0 +1,12 @@
+package com.njusestars.hackthon.enums;
+
+/**
+ * 方法通用返回结果
+ * <br>
+ * created on 2019/5/26
+ *
+ * @author Xunner
+ **/
+public enum Result {
+    SUCCESS, FAILED, NOT_EXIST, EXIST
+}
