a858ad977b0e4c1ede66c1a5d48152bd73b7578b
Xunner <161250042@smail.nju.edu.cn>
2019-05-26 17:23:08 +0800

    backend: update api

--- hackthon/src/main/java/com/njusestars/hackthon/controller/TeacherController.java
+++ hackthon/src/main/java/com/njusestars/hackthon/controller/TeacherController.java
@@ -1,11 +1,16 @@
 package com.njusestars.hackthon.controller;
 
 import com.njusestars.hackthon.bl.TeacherBLService;
+import com.njusestars.hackthon.entity.Assignment;
+import com.njusestars.hackthon.entity.Classroom;
+import com.njusestars.hackthon.entity.Teacher;
 import com.njusestars.hackthon.util.ResultMessage;
 import com.njusestars.hackthon.vo.CreateAssigmentVO;
 import com.njusestars.hackthon.vo.CreateClassroomVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 教师相关Controller
@@ -26,18 +31,36 @@ public class TeacherController {
 
     @PostMapping(value = "/teacher/classroom/create")
     public ResultMessage createClassroom(@RequestBody CreateClassroomVO createClassroomVO) {
-
-        return null;
+        Classroom classroom = new Classroom();
+        classroom.setClassroomName(createClassroomVO.getClassroomName());
+        classroom = teacherBLService.createClassroom(classroom);
+        if (classroom == null) {
+            return new ResultMessage("FAILED", false, null);
+        } else {
+            return this.joinClassroom(createClassroomVO.getTeacherUsername(), classroom.getId());
+        }
     }
 
     @GetMapping(value = "/teacher/classroom/join")
-    public ResultMessage joinClassroom(@RequestParam Long teacherId, @RequestParam Long classroomId) {
+    public ResultMessage joinClassroom(@RequestParam String teacherUsername, @RequestParam Long classroomId) {
+//        Teacher teacher = teacherBLService.joinClassroom(classroomId, teacherUsername);
+//        if (teacher == null) {
+//            return new ResultMessage("FAILED", false, null);
+//        } else {
+//            return new ResultMessage(null, true, null);
+//        }
         return null;
     }
 
     @GetMapping(value = "/teacher/classroom/get-all")
     public ResultMessage getAllClassroom() {
-        return null;
+        List<Classroom> classrooms = teacherBLService.getAllClassroom();
+//        List<ClassroomVO>
+        if (classrooms == null) {
+            return new ResultMessage("FAILED", false, null);
+        } else {
+            return new ResultMessage(null, true, classrooms);
+        }
     }
 
     @GetMapping(value = "/teacher/classroom/my")
@@ -47,6 +70,7 @@ public class TeacherController {
 
     @PostMapping(value = "/teacher/assignment/create")
     public ResultMessage createAssignment(@RequestBody CreateAssigmentVO createAssigmentVO) {
+        Assignment assignment = new Assignment();
         return null;
     }
 
---
+++ hackthon/src/main/java/com/njusestars/hackthon/vo/ClassroomVO.java
@@ -0,0 +1,11 @@
+package com.njusestars.hackthon.vo;
+
+/**
+ *
+ * <br>
+ * created on 2019/5/26
+ *
+ * @author Xunner
+ **/
+public class ClassroomVO {
+}
