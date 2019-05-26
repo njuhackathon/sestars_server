19cc92fa124016e11464ce67f01fe98f2fd59f38
Xunner <161250042@smail.nju.edu.cn>
2019-05-26 20:20:19 +0800

    backend: add maven

---
+++ hackthon/src/test/java/com/njusestars/hackthon/controller/TeacherControllerTest.java
@@ -0,0 +1,40 @@
+package com.njusestars.hackthon.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * 测试
+ * <br>
+ * created on 2019/5/26
+ *
+ * @author Xunner
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeacherControllerTest {
+
+    @Test
+    public void createClassroom() {
+    }
+
+    @Test
+    public void joinClassroom() {
+    }
+
+    @Test
+    public void getAllClassroom() {
+    }
+
+    @Test
+    public void getMyClassroom() {
+    }
+
+    @Test
+    public void createAssignment() {
+    }
+}
