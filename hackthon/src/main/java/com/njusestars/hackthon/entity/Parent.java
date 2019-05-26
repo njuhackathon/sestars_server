8c507e8db10c1a78b1aabb18bfe34e862ac7cad0
njulzb <hellolizebin@foxmail.com>
2019-05-26 10:23:50 +0800

    实体类基本注解

--- hackthon/src/main/java/com/njusestars/hackthon/entity/Parent.java
+++ hackthon/src/main/java/com/njusestars/hackthon/entity/Parent.java
@@ -4,11 +4,13 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity(name = "parent")
 public class Parent extends User {
+    @ManyToMany
     private Set<Student> childrenSet;
 }
--- hackthon/src/main/java/com/njusestars/hackthon/entity/Student.java
+++ hackthon/src/main/java/com/njusestars/hackthon/entity/Student.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
@@ -11,6 +12,7 @@ import java.util.Set;
 @Entity(name = "student")
 public class Student extends User {
     private Classroom classroom;
-    
+
+    @ManyToMany
     private Set<Parent> parents;
 }
