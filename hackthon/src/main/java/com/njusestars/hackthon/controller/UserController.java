494f61ebcc42d540de9d2e8e741eb4a577e3681b
Xunner <161250042@smail.nju.edu.cn>
2019-05-26 10:16:18 +0800

    backend: fix package names

--- hackthon/src/main/java/com/njusestars/hackthon/controller/UserController.java
+++ hackthon/src/main/java/com/njusestars/hackthon/controller/UserController.java
@@ -1,17 +1,19 @@
 package com.njusestars.hackthon.controller;
 
+import com.njusestars.hackthon.util.ResultMessage;
+import com.njusestars.hackthon.vo.LoginVO;
+import com.njusestars.hackthon.vo.SignUpVO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import util.ResultMessage;
-import vo.LoginVO;
 
 public class UserController {
     @PostMapping(value = "/user/login")
     public ResultMessage login(@RequestBody LoginVO loginVO) {
         return null;
     }
+
     @PostMapping(value = "/user/sign-up")
-    public ResultMessage signUp(@RequestBody LoginVO loginVO) {
+    public ResultMessage signUp(@RequestBody SignUpVO signUpVO) {
         return null;
     }
 }
