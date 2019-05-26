1fdf8b8b5d02a57dc94fd26fe09caf32fcca389e
njulzb <hellolizebin@foxmail.com>
2019-05-26 10:58:24 +0800

    修改了pom配置

--- hackthon/src/main/java/com/njusestars/hackthon/entity/Classroom.java
+++ hackthon/src/main/java/com/njusestars/hackthon/entity/Classroom.java
@@ -3,12 +3,15 @@ package com.njusestars.hackthon.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.Set;
 
 @Data
 @Entity(name = "classroom")
 public class Classroom {
+
+    @Id
     private Long id;
 
     @OneToMany
