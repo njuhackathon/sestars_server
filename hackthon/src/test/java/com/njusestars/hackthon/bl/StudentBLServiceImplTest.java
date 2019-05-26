38317bf3aab6e05aadf52aa4fa1ee18df5422585
njulzb <hellolizebin@foxmail.com>
2019-05-26 22:40:41 +0800

    开始student接口测试

---
+++ hackthon/src/test/java/com/njusestars/hackthon/bl/StudentBLServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.njusestars.hackthon.bl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author lzb
+ * @date 2019/5/26 22:40
+ */
+public class StudentBLServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void commitAssignment() {
+    }
+
+    @Test
+    public void getToDoAssignmentList() {
+    }
+
+    @Test
+    public void getStudentByUsername() {
+    }
+
+    @Test
+    public void getQuestionById() {
+    }
+
+    @Test
+    public void joinClassroom() {
+    }
+}
