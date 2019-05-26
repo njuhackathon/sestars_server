ae838333da220731d83e5ef9ae774e29259f3a4f
Xunner <161250042@smail.nju.edu.cn>
2019-05-26 20:12:00 +0800

    backend: complete the Test of UserController

--- hackthon/src/main/java/com/njusestars/hackthon/vo/SignUpVO.java
+++ hackthon/src/main/java/com/njusestars/hackthon/vo/SignUpVO.java
@@ -9,4 +9,14 @@ public class SignUpVO {
     public String password;
     /** 0:教师，1:学生，2:家长 */
     public int type;
+
+    public SignUpVO() {
+    }
+
+    public SignUpVO(String username, String realName, String password, int type) {
+        this.username = username;
+        this.realName = realName;
+        this.password = password;
+        this.type = type;
+    }
 }
