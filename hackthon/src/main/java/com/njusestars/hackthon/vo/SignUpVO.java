94f512595c1595111a1c0185fe42d336463e8a67
Xunner <161250042@smail.nju.edu.cn>
2019-05-26 12:29:54 +0800

    backend: remove useless dependency

--- hackthon/src/main/java/com/njusestars/hackthon/vo/SignUpVO.java
+++ hackthon/src/main/java/com/njusestars/hackthon/vo/SignUpVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class SignUpVO {
     public String username;
-    public String realname;
+    public String realName;
     public String password;
     /** 0:教师，1:学生，2:家长 */
     public int type;
