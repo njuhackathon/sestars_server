8298e1784e8672e4af7474ed994987cc84ad56c8
njulzb <hellolizebin@foxmail.com>
2019-05-26 09:52:23 +0800

    项目基础框架

---
+++ hackthon/src/test/java/com/njusestars/hackthon/HackthonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.njusestars.hackthon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HackthonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
